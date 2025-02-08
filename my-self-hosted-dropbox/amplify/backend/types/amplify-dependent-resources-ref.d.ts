f08b0bfbf84b0c1217e81e16fb9c165412129434
Prakash Shrestha <prakashroj@gmail.com>
2025-02-07 22:49:18 -0500

    some changes in the configurations

--- my-self-hosted-dropbox/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ my-self-hosted-dropbox/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,10 +3,8 @@ export type AmplifyDependentResourcesAttributes = {
     "myselfhosteddropbox53f9692e": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
